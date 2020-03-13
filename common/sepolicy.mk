84d5dcf41021bd6d46c2ece809371436f610b8b0
Jake Weinstein <jake@aospa.co>
2020-03-13 03:41:05 +0100

    sepolicy: add system-only directory to common and qcom
    Change-Id: Ic32d6e86e2796225b894ca5a56280773036c2cbf

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/common/dynamic
+    device/pa/sepolicy/common/dynamic \
+    device/pa/sepolicy/common/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/common/dynamic \
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -14,7 +14,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/pa/sepolicy/qcom/dynamic
+    device/pa/sepolicy/qcom/dynamic \
+    device/pa/sepolicy/qcom/system-only
 else
 BOARD_SEPOLICY_DIRS += \
     device/pa/sepolicy/qcom/dynamic \
